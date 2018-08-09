9e05f2a19f4ea53b6d800b68c2a5e9818d31f087
ZiheLiu <ziheliu1024@gmail.com>
2018-08-09 21:15:34 +0800

    raise ResponseStatusError when res.status_code is not 2000

--- request/request.py
+++ request/request.py
@@ -7,6 +7,17 @@ from .bucket import Bucket
 _bucket = Bucket()
 
 
+class ResponseStatusError(Exception):
+    pass
+
+
+def _get_err_msg(res):
+    try:
+        return res.text()
+    except TypeError:
+        return res.json()
+
+
 def get(url: str, headers=None):
     _bucket.get()
 
@@ -14,5 +25,8 @@ def get(url: str, headers=None):
     res = requests.get(constants.GITHUB_DOMAIN + url,
                        headers=headers,
                        auth=(constants.USERNAME, constants.PASSWORD))
+
+    if res.status_code != 200:
+        raise ResponseStatusError(_get_err_msg(res))
 
     return res
